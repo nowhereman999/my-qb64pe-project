4b9a39e6118cf77b992d5065461c7128a7ad8b31
nowhereman999 <nowhereman999@yahoo.com>
2025-11-15 09:22:17 -0500

    Updated

--- BASIC-To-6809_Source_Code/IDE/compile.bat
+++ BASIC-To-6809_Source_Code/IDE/compile.bat
@@ -1,93 +1,149 @@
 @echo off
-setlocal enabledelayedexpansion
+setlocal EnableDelayedExpansion
 
-REM Initialize variables
-set target=
-set FILE=
-set FILENOEXT=
+rem ------------------------------------------------------------------
+rem  Helper: get the base name of the file (without extension)
+rem ------------------------------------------------------------------
+set "FILENOEXT=%~n1"
 
-REM Parse arguments
-:parse
-if "%~1"=="" goto done
-echo %~1 | findstr "=" >nul
-if %errorlevel%==0 (
-    for /f "tokens=1,2 delims==" %%A in ("%~1") do (
-        set "%%A=%%B"
-    )
-) else (
-    set target=%~1
-)
-shift
-goto parse
-:done
+rem ------------------------------------------------------------------
+rem  Default target (called with: build.bat defaultF11 MYFILE.BAS)
+rem ------------------------------------------------------------------
+if /i "%~1"=="defaultF11" goto :defaultF11
+if /i "%~1"=="Command1"   goto :Command1
+if /i "%~1"=="Command2"   goto :Command2
+if /i "%~1"=="Command3"   goto :Command3
+if /i "%~1"=="Command4"   goto :Command4
+if /i "%~1"=="Command5"   goto :Command5
+if /i "%~1"=="Clean"      goto :Clean
 
-IF "%target%"=="defaultF11" GOTO default
-IF "%target%"=="Command1" GOTO cmd1
-IF "%target%"=="Command2" GOTO cmd2
-IF "%target%"=="Command3" GOTO cmd3
-IF "%target%"=="Command4" GOTO cmd4
-IF "%target%"=="Command5" GOTO cmd5
+echo Usage: %~nx0 ^<target^> [FILE]
+echo.
+echo Targets:
+echo   defaultF11  - full 2 MB CoCo 3 build + emulator start
+echo   Command1    - 64 k CoCo 1 build with Super Loader
+echo   Command2    - simple compile + disk copy (no loader)
+echo   Command3    - same as Command2
+echo   Command4    - compile with -b1 (smaller/faster binary)
+echo   Command5    - compile + disk copy + optional SD-card copy
+echo   Clean       - delete all .bin/.BIN files
+goto :eof
 
-ECHO Unknown target: %target%
-GOTO end
+rem ------------------------------------------------------------------
+rem  defaultF11
+rem ------------------------------------------------------------------
+:defaultF11
+echo Processing %2
+call :SetFile %2
 
-:default
 echo Compiling BAS to ASM...
-./BasTo6809.exe -ascii %FILE%
+BasTo6809.exe -ascii %FILE%
 
 echo Assembling with LWASM...
-./lwasm.exe -9bl -p cd -o./%FILENOEXT%.bin %FILENOEXT%.asm > NEW_Assembly_Listing.txt
+lwasm.exe -9bl -p cd -o./%FILENOEXT%.bin %FILENOEXT%.asm > NEW_Assembly_Listing.txt
 
 echo Copying blank disk image...
 copy /Y BLANK.DSK DISK1.DSK >NUL
 
 echo Copying binary to disk image...
-./decb.exe copy -2 -b -r %FILENOEXT%.bin DISK1.DSK,%FILENOEXT%.BIN
+decb.exe copy -2 -b -r %FILENOEXT%.bin DISK1.DSK,%FILENOEXT%.BIN
 
 rem Uncomment the line below to start MAME (adjust path if needed)
 rem start "" mame.exe -debug -window -r 1280x800 coco3h -ramsize 2M -flop1 ./DISK1.DSK -throttle -frameskip 0 -autoboot_command "\n\n\n\nLOADM\"%FILENOEXT%\":EXEC\n" -ui_active -natural
-GOTO end
 
-:cmd1
-ECHO Running Command1...
-REM Compile program %FILE%
-./BasTo6809 -ascii %FILE%
-GOTO end
+goto :eof
 
-:cmd2
-ECHO Running Command2...
-REM Compile program %FILE%
-./BasTo6809 -ascii %FILE%
-REM Use LWASM to assemble %FILENOEXT%.asm into a CoCo LOADMable program called %FILENOEXT%.bin
-	lwasm -9bl -p cd -o.\%FILENOEXT%.bin %FILENOEXT%.asm > .\NEW_Assembly_Listing.txt
-GOTO end
+rem ------------------------------------------------------------------
+rem  Command1  (64 k CoCo 1 + Super Loader)
+rem ------------------------------------------------------------------
+:Command1
+echo Processing %2
+call :SetFile %2
 
-:cmd3
-ECHO Running Command3...
-REM Compile program %FILE%
-./BasTo6809 -ascii %FILE%
-REM Use LWASM to assemble %FILENOEXT%.asm into a CoCo LOADMable program called %FILENOEXT%.bin
-	lwasm -9bl -p cd -o.\%FILENOEXT%.bin %FILENOEXT%.asm > .\NEW_Assembly_Listing.txt
-REM Compress big >32k files and name if GO.BIN
-REM	-rm GO.BIN
-	.\cc1sl -l %FILENOEXT%.bin -oGO.BIN
-GOTO end
+BasTo6809.exe -ascii %FILE%
+lwasm.exe -9bl -p cd -o./%FILENOEXT%.bin %FILENOEXT%.asm > NEW_Assembly_Listing.txt
 
-:cmd4
-ECHO Running Command4...
-REM Compile program %FILE%
-./BasTo6809 -ascii %FILE%
-REM Use LWASM to assemble %FILENOEXT%.asm into a CoCo LOADMable program called %FILENOEXT%.bin
-	lwasm -9bl -p cd -o.\%FILENOEXT%.bin %FILENOEXT%.asm > .\NEW_Assembly_Listing.txt
-GOTO end
+if exist GO.BIN del /F /Q GO.BIN
+cc1sl.exe -l %FILENOEXT%.bin -oGO.BIN
+if exist %FILENOEXT%.bin del /F /Q %FILENOEXT%.bin
+ren GO.BIN %FILENOEXT%.bin
 
-:cmd5
-ECHO Running Command5...
-REM Compile program %FILE%
-./BasTo6809 -ascii %FILE%
-REM Use LWASM to assemble %FILENOEXT%.asm into a CoCo LOADMable program called %FILENOEXT%.bin
-	lwasm -9bl -p cd -o.\%FILENOEXT%.bin %FILENOEXT%.asm > .\NEW_Assembly_Listing.txt
-GOTO end
+copy /Y BLANK.DSK DISK1.DSK >NUL
+decb.exe copy -2 -b -r %FILENOEXT%.bin DISK1.DSK,%FILENOEXT%.BIN
+goto :eof
 
-:end
-endlocal
+rem ------------------------------------------------------------------
+rem  Command2 (simple compile + disk copy)
+rem ------------------------------------------------------------------
+:Command2
+echo Processing %2
+call :SetFile %2
+
+BasTo6809.exe -ascii %FILE%
+lwasm.exe -9bl -p cd -o./%FILENOEXT%.bin %FILENOEXT%.asm > NEW_Assembly_Listing.txt
+
+copy /Y BLANK.DSK DISK1.DSK >NUL
+decb.exe copy -2 -b -r %FILENOEXT%.bin DISK1.DSK,%FILENOEXT%.BIN
+goto :eof
+
+rem ------------------------------------------------------------------
+rem  Command3  (simple compile + disk copy)
+rem ------------------------------------------------------------------
+:Command3
+echo Processing %2
+call :SetFile %2
+
+BasTo6809.exe -ascii %FILE%
+lwasm.exe -9bl -p cd -o./%FILENOEXT%.bin %FILENOEXT%.asm > NEW_Assembly_Listing.txt
+
+copy /Y BLANK.DSK DISK1.DSK >NUL
+decb.exe copy -2 -b -r %FILENOEXT%.bin DISK1.DSK,%FILENOEXT%.BIN
+goto :eof
+
+rem ------------------------------------------------------------------
+rem  Command4  (-b1 for smaller binary)
+rem ------------------------------------------------------------------
+:Command4
+echo Processing %2 (with -b1)
+call :SetFile %2
+
+BasTo6809.exe -ascii -b1 %FILE%
+lwasm.exe -9bl -p cd -o./%FILENOEXT%.bin %FILENOEXT%.asm > NEW_Assembly_Listing.txt
+
+copy /Y BLANK.DSK DISK1.DSK >NUL
+decb.exe copy -2 -b -r %FILENOEXT%.bin DISK1.DSK,%FILENOEXT%.BIN
+goto :eof
+
+rem ------------------------------------------------------------------
+rem  Command5  (compile + disk + optional SD-card copy)
+rem ------------------------------------------------------------------
+:Command5
+echo Processing %2
+call :SetFile %2
+
+BasTo6809.exe -ascii %FILE%
+lwasm.exe -9bl -p cd -o./%FILENOEXT%.bin %FILENOEXT%.asm > NEW_Assembly_Listing.txt
+
+copy /Y BLANK.DSK DISK1.DSK >NUL
+decb.exe copy -2 -b -r %FILENOEXT%.bin DISK1.DSK,%FILENOEXT%.BIN
+
+rem ----- OPTIONAL: copy to an SD card mounted as drive S: -----
+rem if exist S:\ (
+rem     copy /Y DISK1.DSK S:\DISK1.DSK
+rem     echo DISK1.DSK copied to SD card (S:)
+rem ) else (
+rem     echo SD card not found (drive S:). Skipping copy.
+rem )
+goto :eof
+
+rem ------------------------------------------------------------------
+rem  Subroutine: set FILE and FILENOEXT from the passed argument
+rem ------------------------------------------------------------------
+:SetFile
+set "FILE=%~1"
+if not defined FILE (
+    echo *** ERROR: No file specified.
+    exit /b 1
+)
+set "FILENOEXT=%~n1"
+goto :eof
--- tools/qb64pe_v4.1.0/internal/temp/recompile_osx.command
+++ tools/qb64pe_v4.1.0/internal/temp/recompile_osx.command
@@ -1,5 +1,5 @@
 cd "$(dirname "$0")"
 echo "Recompiling..."
 cd ../c
-make DEP_SOCKETS=y DEP_HTTP=y DEP_ICON=y DEP_FONT=y DEP_ZLIB=y DEP_CONSOLE=y DEP_ICON_RC=y EXE='/Users/glenhewlett/My Drive (nowhereman999@gmail.com)/Programming/CoCo_Related/BASIC-To-6809_V4.43/IDE/SDECB' "CXXFLAGS_EXTRA=" "CFLAGS_EXTRA=" "CXXLIBS_EXTRA=  " -j"10" OS=osx
+make DEP_CONSOLE=y EXE='/Users/glenhewlett/My Drive (nowhereman999@gmail.com)/Programming/CoCo_Related/BASIC-To-6809_V4.43/cc1sl' "CXXFLAGS_EXTRA=" "CFLAGS_EXTRA=" "CXXLIBS_EXTRA=  " -j"10" OS=osx
 read -p "Press ENTER to exit..."
